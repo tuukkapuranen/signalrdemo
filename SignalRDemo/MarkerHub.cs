504b788d93f17bfe10fa4fe6424b0ebbcb8598b5
Tuukka Puranen <tuukka.puranen@nfleet.fi>
2014-07-18 09:55:21 +0300

    Initial commit

---
+++ SignalRDemo/MarkerHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace SignalRDemo
+{
+    // ReSharper disable UnusedMember.Global
+    // ReSharper disable ClassNeverInstantiated.Global
+
+    public class MarkerHub : Hub
+    {
+        private static MarkerModel _theMarker = new MarkerModel { Latitude = 62.25, Longitude = 25.75 };
+
+        public void UpdateModel( MarkerModel clientModel )
+        {
+            _theMarker = clientModel;
+            Clients.AllExcept( Context.ConnectionId ).updateMarker( clientModel );
+        }
+
+        public void RequestUpdate()
+        {
+            Clients.Caller.updateMarker( _theMarker );
+        }
+    }
+
+    public class MarkerModel
+    {
+        [JsonProperty( "latitude" )]
+        public double Latitude { get; set; }
+        [JsonProperty( "longitude" )]
+        public double Longitude { get; set; }
+    }
+}
---
+++ SignalRDemo/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using SignalRDemo;
+
+// ReSharper disable UnusedMember.Global
+
+[assembly: OwinStartup( typeof( Startup ) )]
+namespace SignalRDemo
+{
+    public class Startup
+    {        
+        public void Configuration( IAppBuilder app )
+        {
+            app.MapSignalR();
+        }
+    }
+}
